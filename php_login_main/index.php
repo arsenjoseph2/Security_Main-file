b47f9bed882ab0d8a67a0bc9afc189b7ff1394ca
arsenjoseph2 <118660743+arsenjoseph2@users.noreply.github.com>
2022-11-20 18:02:07 +0800

    Add files via upload

---
+++ php_login_main/index.php
@@ -0,0 +1,58 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+    <meta charset="UTF-8">
+	
+
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    
+    <h1>Data Management System</h1>
+	
+	
+    <?php if (isset($user)): ?>
+	
+     
+        <p>Hello <?= htmlspecialchars($user["name"]) ?></p>
+        
+        <p><a href="logout.php">Log out</a></p>
+		
+     
+    <?php else: ?>
+        
+        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+        
+    <?php endif; ?>
+    
+</body>
+</html>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
